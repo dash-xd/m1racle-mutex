c6650f56fdfee3deb680827d630c5d42ccc9dc28
dev@dashxd <125025267+dash-xd@users.noreply.github.com>
2024-03-18 06:15:53 -0700

    Update mutex.c

--- source-c/mutex.c
+++ source-c/mutex.c
@@ -1,11 +1,37 @@
-// mutex.c
+/*
+ * m1racle-poc: a basic proof of concept for the M1RACLES vulnerability in the Apple M1.
+ * 
+ * This program allows you to read and write the state of the s3_5_c15_c10_1 CPU register.
+ *
+ * Please visit m1racles.com for more information.
+ *
+ * Licensed under the MIT license.
+ */
 
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
 #include <unistd.h>
-#include <stdbool.h>
 
-static unsigned long readers = 0;
-static unsigned long writers = 0;
-static unsigned long writerWaiting = 0;
+// Function to lock mutex
+void miMutexLock() {
+    while (_miread() != 0) { usleep(10); }
+    _miwrite(1);
+}
+
+// Function to unlock mutex
+void miMutexUnlock() {
+    _miwrite(0);
+}
+
+// Extern C functions for accessing the M1RACLES register
+extern void _miwrite(uint64_t value);
+extern uint64_t _miread();
+
+// Reader-Writer Lock
+static uint64_t readers = 0;
+static uint64_t writers = 0;
+static uint64_t writerWaiting = 0;
 
 void miReaderLock() {
     while (writers != 0 || writerWaiting != 0) {
@@ -31,12 +57,13 @@ void miWriterUnlock() {
     writers--;
 }
 
-static unsigned long barrierCounter = 0;
-static unsigned long barrierLimit = 0;
-static unsigned long barrierWaiting = 0;
-static unsigned long barrierMutex = 0;
+// Barrier Synchronization
+static uint64_t barrierCounter = 0;
+static uint64_t barrierLimit = 0;
+static uint64_t barrierWaiting = 0;
+static uint64_t barrierMutex = 0;
 
-void miBarrierInit(unsigned long limit) {
+void miBarrierInit(uint64_t limit) {
     barrierLimit = limit;
 }
 
@@ -45,23 +72,20 @@ void miBarrier() {
     if (barrierCounter == barrierLimit - 1) {
         barrierCounter = 0;
         barrierMutex--;
-        while (barrierWaiting > 0) {
-            usleep(10);
-        }
+        while (barrierWaiting > 0) { usleep(10); }
     } else {
         barrierCounter++;
         barrierMutex--;
         barrierWaiting++;
-        while (barrierCounter != 0) {
-            usleep(10);
-        }
+        while (barrierCounter != 0) { usleep(10); }
         barrierWaiting--;
     }
 }
 
-static unsigned long semCount = 0;
-static unsigned long semMutex = 0;
-static unsigned long semWaiting = 0;
+// Semaphore
+static uint64_t semCount = 0;
+static uint64_t semMutex = 0;
+static uint64_t semWaiting = 0;
 
 void miSemaphore(int count) {
     semCount = count;
@@ -75,9 +99,7 @@ void miSemaphoreWait() {
     } else {
         semWaiting++;
         semMutex--;
-        while (semCount == 0) {
-            usleep(10);
-        }
+        while (semCount == 0) { usleep(10); }
         semWaiting--;
         semCount--;
     }
@@ -93,48 +115,64 @@ void miSemaphoreSignal() {
     semMutex--;
 }
 
-static bool eventFlag = false;
-static unsigned long eventMutex = 0;
-static unsigned long eventWaiting = 0;
+// Event Handling
+static int eventFlag = 0;
+static uint64_t eventMutex = 0;
+static uint64_t eventWaiting = 0;
 
 void miEventWait() {
     eventMutex++;
     if (!eventFlag) {
         eventWaiting++;
         eventMutex--;
-        while (!eventFlag) {
-            usleep(10);
-        }
+        while (!eventFlag) { usleep(10); }
         eventMutex++;
         eventWaiting--;
     }
-    eventFlag = false;
+    eventFlag = 0;
     eventMutex--;
 }
 
 void miEventSignal() {
     eventMutex++;
-    eventFlag = true;
+    eventFlag = 1;
     if (eventWaiting > 0) {
         eventWaiting--;
     }
     eventMutex--;
 }
 
+// Transaction Control
 void miTransactionalMemory() {
-    // Simulate transactional memory by sleeping for a short duration
+    miMutexLock();
+    // Perform atomic memory operations here
+    // For simplicity, let's just sleep for a short duration to simulate transactional memory
     usleep(100);
+    miMutexUnlock();
 }
 
-static bool distributedLock = false;
+// Distributed Coordination
+static int distributedLock = 0;
 
 void miDistributedCoordination() {
     while (distributedLock) {
         usleep(10);
     }
-    distributedLock = true;
+    distributedLock = 1;
 }
 
 void miReleaseDistributedLock() {
-    distributedLock = false;
+    distributedLock = 0;
+}
+
+int main(int argc, char **argv) {
+    uint64_t val;
+    if (argc > 1) {
+        val = atoi(argv[1]);
+        _miwrite(val);
+    } else {
+        val = _miread();
+        printf("%llu\n", val);
+    }
+    return 0;
 }
